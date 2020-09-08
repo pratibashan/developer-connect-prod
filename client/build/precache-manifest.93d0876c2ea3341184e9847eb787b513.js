17afd64cf59154a5e81c32c258e2a75117b8285a
Pratiba Shan <pratibashan.g@gmail.com>
2020-09-08 15:17:21 -0500

    Add client folder back to git repo

---
+++ client/build/precache-manifest.93d0876c2ea3341184e9847eb787b513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934335ebddc36785eec020f35dc13882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d074cfecdf389e3dbe",
+    "url": "/static/css/main.2ace803b.chunk.css"
+  },
+  {
+    "revision": "8d051b832ef5d5418551",
+    "url": "/static/js/2.f2649386.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.f2649386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d074cfecdf389e3dbe",
+    "url": "/static/js/main.a5827ead.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
