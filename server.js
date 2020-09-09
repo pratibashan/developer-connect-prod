377861f1aec76c49f299e1ca40724905b0e25b2a
Pratiba Shan <pratibashan.g@gmail.com>
2020-09-09 14:15:14 -0500

    changed port in serverjs

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
